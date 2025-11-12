51d4f356d9339af0c6066b531e73df9bf51dc36d
Brunnn <szbrunoabreu@gmail.com>
2025-11-12 09:01:17 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+function modoEscuro(){
+    var element = document.body;
+    element.classList.toggle("text-light");
+    element.classList.toggle("btn-outline-light");
+    element.classList.toggle("bg-dark");
+}
